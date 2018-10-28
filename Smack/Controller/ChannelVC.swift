5902f22cbaf57ce6c91c6bfcd431681ba3d75768
Paul Hofer <paulhofer@pauls-macbook-pro.local>
2018-10-28 15:37:33 +0100

    Setup  Project including SWReveal

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChannelVC.swift
 //  Smack
 //
 //  Created by Paul Hofer on 28.10.18.
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
 
 }
-
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Paul Hofer on 28.10.18.
+//  Copyright © 2018 Hopeli. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //    MARK: - Outlets
+    
+    @IBOutlet weak var menuButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+    
+
+
+}
